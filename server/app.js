c1825dca2d9e1714b2b398cd0e57cf831680ed18
Kin2808 <kin0905152701@gmail.com>
2023-02-03 13:19:27 +0700

    .

--- server/app.js
+++ server/app.js
@@ -29,16 +29,14 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+app.use(cors({ origin: "*" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use(bodyParser.json({ limit: "30mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
-app.use(cors({ origin: "*" }));
 
 app.use("/blog", blogRouter);
 
