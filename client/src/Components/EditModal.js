cbca6ec85f426aec663c117d3ba9ba36d0851c5d
Kin2808 <kin0905152701@gmail.com>
2023-02-04 11:54:38 +0700

    .

--- client/src/Components/EditModal.js
+++ client/src/Components/EditModal.js
@@ -4,7 +4,7 @@ import { MdDeleteOutline } from "react-icons/md";
 
 const EditModal = ({ updateBlog, deleteBlog }) => {
   return (
-    <div className="absolute bg-background right-0 top-7 bg-transparent border rounded-xl">
+    <div className="absolute bg-background right-0 top-7 border rounded-xl">
       <div
         className="flex justify-between items-center px-5 py-2 border-b duration-200 opacity-70 hover:opacity-100 cursor-pointer"
         onClick={updateBlog}
